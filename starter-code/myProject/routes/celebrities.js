35517a220e97ae95916da6df047f9431ca069f77
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-02-15 22:26:56 -0600

    iteration 3 almost done

--- starter-code/myProject/routes/celebrities.js
+++ starter-code/myProject/routes/celebrities.js
@@ -5,9 +5,15 @@ const Celebrity = require("../models/Celebrity");
 //mi ruta
 router.get("/", (req,res)=>{
   Celebrity.find({}, (err, data)=>{
-    if(err) res.status(500).send(err);
-    console.log(data);
+    // if(err) res.status(500).send(err);
     res.render("./celebrities/index", { celebrities:data });
+  });
+});
+
+router.get("/celebrities/:id", (req,res)=>{
+  const id = req.params.id;
+  Celebrity.findById(id, (err, data)=>{
+    res.render("./celebrities/show",{celebrities:data});
   });
 });
 
